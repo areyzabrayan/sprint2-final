0a20a08437ec57f9c481b6dd89538fa9787bb4ea
Brayan Areiza <areizabrayan11@gmail.com>
2023-07-24 22:36:11 -0500

    update info

--- src/components/adminPanel/AdminPanel.jsx
+++ src/components/adminPanel/AdminPanel.jsx
@@ -5,6 +5,7 @@ import Categories from "../banner/categories/Categories";
 import Cards from "../cards/cards";
 import './adminPanel.scss'
 import AdminEdit from "../adminEdit/adminEdit";
+import Boletos from "../boletos/boletos";
 
 const AdminPanel = ({ signIn }) => {
   return (
@@ -23,7 +24,9 @@ const AdminPanel = ({ signIn }) => {
     </div>
       <button onClick={() => signIn(false)}>Cerrar sesion</button>;
       {/* <Cards/> */}
-      <AdminEdit/>
+      {/* <AdminEdit/> */}
+      <Boletos/>
+      {/* <BuyTikecks/> */}
     </>
   );
 };

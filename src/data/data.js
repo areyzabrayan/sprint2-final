47c88ce3882429b34e561bff06f530e5db1adb0c
Alexis Garcia <alexis25yo@gmail.com>
2023-07-30 13:28:16 -0500

    new repo

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ const URL_API_MOVIE = "https://api.themoviedb.org/3/movie/";
 const API_KEY = "f9f10c64c7bfff66c78009f93441364c";
 const URL_IMG = "https://image.tmdb.org/t/p/w500/";
 const URL_SERVER = "http://localhost:5000/";
-
+//gola
 export const endPoints = {
   urlCinemas: `${URL_SERVER}cinemas`,
   urlFunctions: (id) => `${URL_SERVER}funciones/${id}`,
